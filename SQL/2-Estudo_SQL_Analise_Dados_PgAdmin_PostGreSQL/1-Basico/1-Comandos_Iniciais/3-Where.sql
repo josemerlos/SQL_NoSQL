539be4a27c50adb52b3632a1f9ee08e8138485c2
José Carlos Merlos Junior <carlosj.info@gmail.com>
2024-05-30 16:32:06 -0300

    dados

---
+++ SQL/2-Estudo_SQL_Analise_Dados_PgAdmin_PostGreSQL/1-Basico/1-Comandos_Iniciais/3-Where.sql
@@ -0,0 +1,53 @@
+-- Serve para filtrar linhas de acordo com uma condição
+
+
+select coluna_1, coluna_2, coluna_3
+from schema_1.tabela_1
+where condição_x=true
+
+
+
+select email,state from sales.customers
+
+select * from sales.customers	
+
+ select distinct state from sales.customers
+
+
+
+-- (Exemplo 1) Filtro com condição única
+-- Liste os emails dos clientes da nossa base que moram no estado de Santa Catarina
+
+
+select email,state from sales.customers where state = 'SC'
+
+
+-- (Exemplo 2) Filtro com mais de uma condição
+-- Liste os emails dos clientes da nossa base que moram no estado de Santa Catarina
+-- ou Mato Grosso do Sul
+
+
+select email,state 
+from sales.customers 
+where state = 'SC' or state = 'MS' order by state
+
+
+-- (Exemplo 3) Filtro de condição com data
+-- Liste os emails dos clientes da nossa base que moram no estado de Santa Catarina 
+-- ou Mato Grosso do Sul e que tem mais de 30 anos
+
+
+
+select email,state,birth_date 
+from sales.customers 
+where (state = 'SC' or state = 'MS')  
+and birth_date < '1991-12-28'   	
+order by state
+
+
+
+-- (1) Comando utilizado para filtrar linhas de acordo com uma condição
+-- (2) No PostgreSQL são utilizadas aspas simples para delimitar strings 
+-- (3) string = sequência de caracteres = texto
+-- (4) Pode-se combinar mais de uma condição utilizando os operadores lógicos
+-- (5) No PostgreSQL as datas são escritas no formato 'YYYY-MM-DD' ou 'YYYYMMDD'
